983a194fb7cda37b2b6edc709dc2bb5b295064a6
Md. Amin Uddin <amintushar3@gmail.com>
2023-05-18 11:27:37 +0600

    main

--- resources/views/filelist-user.blade.php
+++ resources/views/filelist-user.blade.php
@@ -5,12 +5,10 @@
   <td width=50%>{{$file->file_title}}</td>
   <td width=50%>{{$file->catagory}}</td>
   <td >{{$file->file_size}} MB</td>
-  <td width=10%> <button class="btn btn-outline-primary mb-10">
-  <a href="{{route('t.downfile',$file->id)}}">
+  <td width=10%> 
+  <a href="{{route('t.downfile',$file->id)}}" class="btn btn-outline-primary mb-10">
   <i class="bi bi-cloud-download-fill"></i>  Downlod</a></td>
   
-
-  </button> 
 
 </tr>
 @endforeach
--- resources/views/filelist.blade.php
+++ resources/views/filelist.blade.php
@@ -3,10 +3,7 @@
 <tr>
   <td width=10% >{{ $file->id}}</td>
   <td width=50%>{{$file->file_title}}</td>
-<<<<<<< HEAD
   <td width=50%>{{$file->catagory}}</td>
-=======
->>>>>>> 7bbb3642046e60c856f9237782cc8828e96bab86
   <td >{{$file->file_size}} MB</td>
   <td width=10%> <button class="btn btn-outline-primary mb-10">
   <a href="{{route('t.downfile',$file->id)}}">
