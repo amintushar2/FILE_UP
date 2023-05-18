98f3358ac62068b863a10f188afdb148bc044ffe
Md. Amin Uddin <amintushar3@gmail.com>
2023-05-18 11:29:09 +0600

    main

--- resources/views/filelist.blade.php
+++ resources/views/filelist.blade.php
@@ -5,14 +5,11 @@
   <td width=50%>{{$file->file_title}}</td>
   <td width=50%>{{$file->catagory}}</td>
   <td >{{$file->file_size}} MB</td>
-  <td width=10%> <button class="btn btn-outline-primary mb-10">
-  <a href="{{route('t.downfile',$file->id)}}">
+  <td width=10%>
+  <a href="{{route('t.downfile',$file->id)}}"class="btn btn-outline-primary">
   <i class="bi bi-cloud-download-fill"></i>  Downlod</a></td>
-  <td width=10%> <button type="button" class="btn btn-outline-danger">
-  <a href="{{route('f.del',$file->id)}}">
+  <td width=10%>
+  <a href="{{route('f.del',$file->id)}}"class="btn btn-outline-danger">
   Delete</a></td>
-
-  </button> 
-
 </tr>
 @endforeach
